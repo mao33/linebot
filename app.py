4c281d352fccb96be30fca29a3e47433c9f0bc13
mao <mao0125mao@icloud.com>
2018-08-31 20:51:23 +0900

    test

--- app.py
+++ app.py
@@ -40,9 +40,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.massage.text == 'あ':
+        line_bot_api.reply_massage(
+            event.reply_token,
+            TextSendMassage(text='いう'))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
